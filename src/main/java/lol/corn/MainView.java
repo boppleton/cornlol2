85641e786517e40dc76ef3e9edb391a76b36bcae
boppleton <bots@boppleton.com>
2018-04-05 17:15:05 -0600

    add broadcaster

--- src/main/java/lol/corn/MainView.java
+++ src/main/java/lol/corn/MainView.java
@@ -1,26 +1,31 @@
 package lol.corn;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.Command;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import lol.corn.trade.TradeUni;
+import lol.corn.utils.Broadcaster;
+import org.atmosphere.cache.BroadcastMessage;
 
 import java.util.List;
 
 
-
+@Push
 @BodySize(height = "100vh", width = "100vw")
 @HtmlImport("styles/shared-styles.html")
 @Route("")
 @Theme(Lumo.class)
-public class MainView extends SplitLayout {
+public class MainView extends SplitLayout implements Broadcaster.BroadcastListener {
 
     private static SplitLayout mainSplit = new SplitLayout();
     private static Grid<TradeUni> tradesGrid = new Grid<>();
@@ -30,11 +35,15 @@ public class MainView extends SplitLayout {
         setupLayout();
 
 
+
         tradesGrid.addColumn(TradeUni::getPair).setHeader("pair").setResizable(true);
         tradesGrid.addColumn(TradeUni::getSize).setHeader("USD value").setResizable(true);
         tradesGrid.addColumn(TradeUni::getPrice).setHeader("BTC price").setResizable(true);
 
 
+
+
+        Broadcaster.register(this);
 
         setClassName("main-layout");
     }
@@ -54,4 +63,16 @@ public class MainView extends SplitLayout {
 
     }
 
+    @Override
+    public void receiveBroadcast(BroadcastMessage message) {
+
+        UI.getCurrent().access(new Command() {
+            @Override
+            public void execute() {
+                System.out.println(message);
+            }
+        });
+
+
+    }
 }
